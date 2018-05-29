b99c177bbe4168406bc826cfbab13b2e7fe98b1e
Shubham Chopra <shubhamchopra553@gmail.com>
2018-05-29 16:32:09 +0530

    Add files via upload

---
+++ hibernate-8/src/main/java/com/hibernate/FourWheeler.java
@@ -0,0 +1,22 @@
+package com.hibernate;
+
+import javax.persistence.Entity;
+
+@Entity
+public class FourWheeler extends Vehicle {
+	
+	private String SteeringWheel;
+
+	public String getSteeringWheel() {
+		return SteeringWheel;
+	}
+
+	public void setSteeringWheel(String steeringWheel) {
+		SteeringWheel = steeringWheel;
+	}
+
+	
+	
+	
+
+}
